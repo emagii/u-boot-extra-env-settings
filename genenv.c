dea6816bf7f20988d6d26ff58112c426a1e3c432
Ulf Samuelsson <ulf@emagii.com>
2023-02-02 21:16:38 +0100

    Add comment and variables
    Comments start with '#'
    Variable definitions start with "#!"
    Characters in Variable names = [a-zA-Z0-9_]
    
    Signed-off-by: Ulf Samuelsson <ulf@emagii.com>

--- genenv.c
+++ genenv.c
@@ -15,6 +15,7 @@
 #include <string.h>
 
 #define	MAX_BUF	  1024*1024
+/* We can only define this number of environment variables */
 #define MAX_NAMES 4096
 #define LF			'\n'
 #define	EOS			'\0'
@@ -29,70 +30,133 @@
 #define	MARKER
 #endif
 
-
 typedef	char *ptr;
+typedef struct {
+	int	pos;		/* Start in buffer for this variable */
+	int last;		/* Start in buffer for next variable */
+	int valid;		/* Has been defined */
+	char name[128];
+} var_t;
+
 char	buffer[MAX_BUF];
 int		run=1;
 
-ptr		names [MAX_NAMES];
-ptr		values[MAX_NAMES];
 
-void emit(char *s)
+
+ptr		names [MAX_NAMES];
+var_t	variables[MAX_NAMES];
+
+int idchar(char c)
 {
-	int c;
-	do {
-		c = *s++;
-		if (c == '\n') {
-			printf("<LF>\n");
-		} else if (c == '\r') {
-			printf("<CR>\n");
-		} else {
-			putchar(c);
-		}
-	} while (c != '\0');
-	putchar(LF);
+	if ((c >= 'a') && (c <= 'z'))
+		return 1;
+	if ((c >= 'A') && (c <= 'Z'))
+		return 1;
+	if ((c >= '0') && (c <= '9'))
+		return 1;
+	if (c == '_')
+		return 1;
+	return 0;
 }
 
+void defvar(var_t *v, int vix)
+{
+	var_t *this = &variables[vix];
+	strcpy(this->name, v->name);
+	this->pos = v->pos;
+	if (this->name[0] != EOS) {
+		this->valid = 1;
+	} else {
+		this->valid = 0;
+	}
+	this->last= -1;
+	if (vix != 0) {
+		variables[vix-1].last = v->pos-1;
+	}
+}
 
 int get_file()
 {
-	int c, size;
+	int c, size, linefeed, skip;
+	int current_var;
+	var_t var;
+	current_var=0;
+	linefeed=1;
+	skip=0;
 	BEGIN;
-	for (size = 0 ; size < MAX_BUF ; size++) {
+	for (size = 0 ; size < MAX_BUF ; ) {
 		c = getchar();
 		if (c == EOF) {
 			buffer[size] = '\n';
 			buffer[size+1] = '\0';
+			/* Define an empty variable at the end */
+			var.name[0] = EOS;
+			var.pos = size + 1;
+			defvar(&var, current_var);
 			END;
 			return size;
+		} else if (skip == 1) {
+			if (c == LF) {
+				linefeed=1;
+				skip=0;
+			}
+			/* if (c == EOF) we will get it again during next getchar() */
+		} else {
+			if (linefeed == 1) {
+				/* Check for comment of variable definition */
+				if (c == '#') {
+					skip=1;
+					c = getchar();
+					if (c == '!') {	/* Define a variable name */
+						int vix = 0;
+						while(1) {
+							c = getchar();
+							if (idchar(c)) {
+								var.name[vix++] = c;
+							} else {
+								break;
+							}
+						}
+						var.name[vix++] = EOS;
+						var.pos = size;
+						defvar(&var, current_var++);
+					}
+				} else {
+					skip=0;
+				}
+			}
+			if (skip != 1) {
+				buffer[size++] = c;
+			}
+			if (c == LF) {
+				linefeed = 1;
+				skip=0;
+			} else {
+				linefeed = 0;
+			}
 		}
-		buffer[size] = c;
 	}
 	END;
 	return size;
 }
 
-int split_file(int size)
+int split_file(int start, int stop)
 {
 	int i;
 	int ix = 0;
 	int c0,c1;
 	BEGIN;
-	i  = 0;
-	ix = 0;
-#if	0
-	if (size > 200) {
-		printf("Size too large: %d\n", size);
-		return 1;
+	i  = start;
+	for (ix = 0; ix < MAX_NAMES ; ix++) {
+		names[ix++] = NULL;
 	}
-#endif
+	ix = 0;
 	if (buffer[i] == EOS) {
 		return 0;
 	} else {
 		names[ix++] = &buffer[i];
 	}
-//	printf("size = %d\n", size);
-	while(i < size) {
+	while(i <= stop) {
 		if (buffer[i] == LF) {
 			int pos = i;
 			while(buffer[i] == LF) {
@@ -112,21 +176,6 @@ int split_file(int size)
 	}
 	END;
 	return ix;
-}
-
-void debug_names(int count)
-{
-#if	defined(DEBUG)
-	int i;
-	for (i = 0 ; i < count ; i++) {
-		if (names[i] == NULL) {
-			printf("%d: NULL\n", i);
-		} else {
-			printf("%d: ", i);
-			emit(names[i]);
-		}
-	}
-#endif
 }
 
 void indent(void)
@@ -170,26 +219,44 @@ void process_var(char *s)
 	} while(c);
 }
 
-void print_env(int count)
+void print_env(char *s, int count)
 {
 	int i;
-	printf("#define CONFIG_EXTRA_ENV_SETTINGS	\\\n");
+	printf("#define %s	\\\n", s);
 	for(i = 0 ; i < count ; i++) {
 		process_var(names[i]);
 	}
 	printf("\n");
 }
 
+void print_var(var_t *v)
+{
+	int i;
+	printf("pos=%-5d-%5d: %s\n", v->pos, v->last, v->name);
+	for (i = v->pos ; i < v->last ; i++) {
+		putchar(buffer[i]);
+	}
+	putchar(LF);
+}
+
+void print_vars(void)
+{
+	int i, count;
+	for (i = 0; i < MAX_NAMES ; i++) {
+		var_t *this;
+		this = &variables[i];
+		if (this->valid) {
+//			print_var(this);
+			count = split_file(this->pos, this->last);
+			print_env(this->name, count);
+		}
+	}
+}
 int main(void)
 {
 	int i, size, count;
 	size = get_file();
 	buffer[size] = EOS;
-	MARKER;
-	count = split_file(size);
-	MARKER;
-//	printf("count = %d\n", count);
-	debug_names(count);
-	print_env(count);
+	print_vars();
 	return 0;
 }
